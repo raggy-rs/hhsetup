a73f0383adadfcd861fb400d78912f801b0d7a63
raggy <sr.raggy@gmx.at>
2014-08-01 15:34:13 +0200

    added hosts dict to settings.py

--- settings.py
+++ settings.py
@@ -5,3 +5,4 @@ sshdir='/home/'+username+'/.ssh/'
 jdktar='/home/'+username+'/jdk-7u45-linux-x64.tar.gz'
 jvmdir='/usr/lib/jvm/'
 jdkversion='jdk1.7.0_45'
+hosts={'10.148.0.15':'hhm','10.148.0.17':'hhs1'}
