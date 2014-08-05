285497213adb0f320d2b3171f7d70e9c254ee423
raggy <sr.raggy@gmx.at>
2014-08-05 19:38:23 +0200

    added start_services.py and fixed 5_hadoop_conf.py

--- 5_hadoop_conf.py
+++ 5_hadoop_conf.py
@@ -4,11 +4,18 @@ import subprocess as sp
 from settings import username, masterip, configname
 
 if __name__ == '__main__':
-        scpstr='scp -r {0}@{1}:/etc/hadoop/conf.{2} /etc/hadoop/conf.{2}'.format(username,masterip,configname)
+        scpstr='scp -r {0}@{1}:/etc/hadoop/conf.{2} /etc/hadoop/'.format(username,masterip,configname)
 	installalt = 'update-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.{} 50'.format(configname)
 	setalt = 'update-alternatives --set hadoop-conf /etc/hadoop/conf.{}'.format(configname)
+	print 'Copy {0}@{1}:/etc/hadoop/conf.{2} to /etc/hadoop/'.format(username,masterip,configname)
 	sp.check_call(shlex.split(scpstr))
+	print 'Update Alternatives'
 	sp.check_call(shlex.split(installalt))
 	sp.check_call(shlex.split(setalt))
-	os.makedirs(os.path.expanduser("~/data/dfs/nn"))
-	os.mkdir(os.path.expanduser("~/data/dfs/dn"))
+	print 'Create data directories if nessecary'
+	nnpath=os.path.expanduser("~/data/dfs/nn")
+	dnpath=os.path.expanduser("~/data/dfs/dn")
+	if not os.path.isdir(nnpath):
+		os.makedirs(nnpath)
+	if not os.path.isdir(dnpath):
+		os.makedirs(dnpath)
