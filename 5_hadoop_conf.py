e7797ace0f0b8b7acb151c765bd09cba1c1f2371
raggy <sr.raggy@gmx.at>
2014-08-07 14:59:22 +0200

    renamed:    4_network_conf.py -> 3_network_conf.py renamed:    3_chd5.py -> 4_chd5.py modified:   5_hadoop_conf.py modified:   README wmodified:   reset.py

--- 5_hadoop_conf.py
+++ 5_hadoop_conf.py
@@ -3,6 +3,9 @@ import os,sys,shlex
 import subprocess as sp
 from settings import username, masterip, configname, hosts
 
+def call(cmdstr):
+	sp.check_call(shlex.split(cmdstr))
+
 if __name__ == '__main__':
 	masterhost = hosts[masterip]
 	print 'Update Alternatives'
@@ -10,21 +13,24 @@ if __name__ == '__main__':
 	installalt = 'update-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.{} 50'.format(configname)
 	setalt = 'update-alternatives --set hadoop-conf /etc/hadoop/conf.{}'.format(configname)
 	print 'Copy {0}@{1}:/etc/hadoop/conf.{2} to /etc/hadoop/'.format(username, masterhost, configname)
-	sp.check_call(shlex.split(scpstr))
-	sp.check_call(shlex.split(installalt))
-	sp.check_call(shlex.split(setalt))
+	call(scpstr)
+	call(installalt)
+	call(setalt)
         
 	scpstr='scp -r {0}@{1}:/etc/hbase/conf.{2} /etc/hbase/'.format(username, masterhost, configname)
 	installalt = 'update-alternatives --install /etc/hbase/conf hbase-conf /etc/hbase/conf.{} 50'.format(configname)
 	setalt = 'update-alternatives --set hbase-conf /etc/hbase/conf.{}'.format(configname)
 	print 'Copy {0}@{1}:/etc/hbase/conf.{2} to /etc/hbase/'.format(username, masterhost, configname)
-	sp.check_call(shlex.split(scpstr))
-	sp.check_call(shlex.split(installalt))
-	sp.check_call(shlex.split(setalt))
+	call(scpstr)
+	call(installalt)
+	call(setalt)
 	print 'Create data directories if nessecary'
-	nnpath=os.path.expanduser("~/data/dfs/nn")
-	dnpath=os.path.expanduser("~/data/dfs/dn")
+	datapath=os.path.expanduser('~/data')
+	nnpath=os.path.join(datapath,'/dfs/nn')
+	dnpath=os.path.join(datapath,'/dfs/dn')
 	if not os.path.isdir(nnpath):
 		os.makedirs(nnpath)
 	if not os.path.isdir(dnpath):
 		os.makedirs(dnpath)
+	
+	call('chown -R hdfs:hdfs '+datapath)
--- reset.py
+++ reset.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
-import os,sys,shlex,shutil
+import os, sys, shlex, socket
 import subprocess as sp
-
+from settings import masterip, hosts,username
 #for x in `cd /etc/init.d ; ls hadoop-hdfs-*` ; do sudo service $x start ; done
 services = ['hadoop-hdfs-namenode','hadoop-yarn-resourcemanager','hbase-master','hbase-regionserver']
 
 def call(argstr):
 	sp.check_call(shlex.split(argstr))
+
+def clear_data_dirs():
+	dfsbase = '/home/{}/data/dfs'.format(username)
+	nndir = os.path.join(dfsbase,'nn')
+	for f in os.listdir(nndir):
+		call('rm -r '+os.path.join(nndir,f))
+	dndir = os.path.join(dfsbase,'dn')
+	for f in os.listdir(dndir):
+		call('rm -r '+os.path.join(dndir,f))	
 
 def clear_log_dirs():
 	logbase='/var/log'
@@ -15,7 +24,9 @@ def clear_log_dirs():
 		if d.startswith('hadoop') or d.startswith('hbase'):
 			logdir = os.path.join(logbase,d)
 			for f in os.listdir(logdir):
-				os.remove(os.path.join(logdir,f))
+				p=os.path.join(logdir,f)
+				if os.path.isfile(p):
+					os.remove(p)
 
 def format_namenode():
 	call('sudo -u hdfs hadoop namenode -format')
@@ -33,6 +44,7 @@ def start_services(services):
 
 
 if __name__ == '__main__':
+	hostname=socket.gethostname()
 	allservices = os.listdir('/etc/init.d')
 	services = filter(lambda x: x.startswith("hadoop-hdfs"),allservices)
 	services.extend(filter(lambda x: x.startswith('hadoop-yarn'),allservices))
@@ -40,7 +52,9 @@ if __name__ == '__main__':
 	services.extend(sorted(filter(lambda x: x.startswith('hbase'),allservices)))
 	print services
 	stop_services(services)
+	clear_data_dirs()
 	clear_log_dirs()
-	format_namenode()
+	if hostname == hosts[masterip]:
+		format_namenode()
 	start_services(services)
 
