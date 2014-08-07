90f3e6a9bdf95db5804bb3998c2ccbc32d2d68d1
cloud <cloud@hhm.(none)>
2014-08-07 20:20:37 +0200

    Added configuration in folder conf and updated 5_hadoop_conf.py to use this configuration 	modified:   5_hadoop_conf.py

--- 5_hadoop_conf.py
+++ 5_hadoop_conf.py
@@ -7,24 +7,15 @@ def call(cmdstr):
 	sp.check_call(shlex.split(cmdstr))
 
 if __name__ == '__main__':
-	masterhost = hosts[masterip]
-	print 'Update Alternatives'
-        scpstr = 'scp -r {0}@{1}:/etc/hadoop/conf.{2} /etc/hadoop/'.format(username, masterhost, configname)
-	installalt = 'update-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.{} 50'.format(configname)
-	setalt = 'update-alternatives --set hadoop-conf /etc/hadoop/conf.{}'.format(configname)
-	print 'Copy {0}@{1}:/etc/hadoop/conf.{2} to /etc/hadoop/'.format(username, masterhost, configname)
-	call(scpstr)
-	call(installalt)
-	call(setalt)
+	confbase = os.path.expanduser('~/hhsetup/conf/')
+	print 'Update Alternatives',
+	for what in ['hadoop','hbase','zookeeper']:
+		print what,
+		confdir = os.path.join(confbase,what)
+		call('update-alternatives --install /etc/{0}/conf {0}-conf {1} 100'.format(what,confdir))
+		call('update-alternatives --set {0}-conf {1}'.format(what, confdir))
         
-	scpstr='scp -r {0}@{1}:/etc/hbase/conf.{2} /etc/hbase/'.format(username, masterhost, configname)
-	installalt = 'update-alternatives --install /etc/hbase/conf hbase-conf /etc/hbase/conf.{} 50'.format(configname)
-	setalt = 'update-alternatives --set hbase-conf /etc/hbase/conf.{}'.format(configname)
-	print 'Copy {0}@{1}:/etc/hbase/conf.{2} to /etc/hbase/'.format(username, masterhost, configname)
-	call(scpstr)
-	call(installalt)
-	call(setalt)
-	print 'Create data directories if nessecary'
+	print '\nCreate data directories if nessecary'
 	datapath=os.path.expanduser('~/data')
 	nnpath=os.path.join(datapath,'/dfs/nn')
 	dnpath=os.path.join(datapath,'/dfs/dn')
---
+++ conf/hadoop/hadoop-env.sh
@@ -0,0 +1,1 @@
+export JAVA_HOME=/usr/lib/jvm/jdk1.7.0_45/
