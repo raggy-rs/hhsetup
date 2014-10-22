e49e3bc40e7e0d6a0bc4ee8efc42a49b5df92771
cloud <cloud@hhm.(none)>
2014-10-22 15:14:41 +0200

    modified:   start_hbase.py

--- start_hbase.py
+++ start_hbase.py
@@ -3,6 +3,6 @@ from reset import start_services,get_services
 
 if __name__=='__main__':
 	services = get_services()
-	services = filter(lambda x: "hadoop" in x, services)
+	services = filter(lambda x: "hadoop" not in x, services)
 	start_services(services)
 
