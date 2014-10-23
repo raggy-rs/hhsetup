b9a873cf0fcd57493cebcfb3db3a9024de4a00bb
cloud <cloud@hhm.(none)>
2014-10-23 17:17:47 +0200

    added utility to clean the classpath

---
+++ clean_hbase_classpath.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import subprocess as sp
+import os
+import glob
+
+classpath=[]
+paths = set([os.path.dirname(os.path.abspath(x)) for x in sp.check_output(["hbase","classpath"]).split(':')])
+
+for p in paths:
+	jars=glob.glob(p+'/*.jar')
+	if len(jars)>0:
+		classpath+=jars
+	else:
+		classpath+=[p]
+print ':'.join(classpath)
+	
